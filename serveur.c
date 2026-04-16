e66104bfef7bfc08a0c71d4d2463591414daeb02
DERUDA Gabriel <gabriel@deruda.fr>
2026-04-16 10:44:36 +0200

    progress serveur

--- serveur.c
+++ serveur.c
@@ -9,63 +9,70 @@
 /*		Auteurs :  ....						      */
 /*		Date :  ....						      */
 /*									      */
-/******************************************************************************/	
+/******************************************************************************/
 
-#include<stdio.h>
 #include <curses.h>
+#include <stdio.h>
 
-#include<sys/signal.h>
-#include<sys/wait.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <sys/signal.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
 
-#include "fon.h"     		/* Primitives de la boite a outils */
+#include "fon.h" /* Primitives de la boite a outils */
 
 #define SERVICE_DEFAUT "1111"
 
-void serveur_appli (char *service);   /* programme serveur */
+void serveur_appli(char *service); /* programme serveur */
 
-
-/******************************************************************************/	
+/******************************************************************************/
 /*---------------- programme serveur ------------------------------*/
 
-int main(int argc,char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-	char *service= SERVICE_DEFAUT; /* numero de service par defaut */
+  char *service = SERVICE_DEFAUT; /* numero de service par defaut */
 
+  /* Permet de passer un nombre de parametre variable a l'executable */
+  switch (argc) {
+  case 1:
+    printf("defaut service = %s\n", service);
+    break;
+  case 2:
+    service = argv[1];
+    break;
 
-	/* Permet de passer un nombre de parametre variable a l'executable */
-	switch (argc)
- 	{
-   	case 1:
-		  printf("defaut service = %s\n", service);
-		  		  break;
- 	case 2:
-		  service=argv[1];
-            break;
+  default:
+    printf("Usage:serveur service (nom ou port) \n");
+    exit(1);
+  }
 
-   	default :
-		  printf("Usage:serveur service (nom ou port) \n");
-		  exit(1);
- 	}
+  /* service est le service (ou numero de port) auquel sera affecte
+  ce serveur*/
 
-	/* service est le service (ou numero de port) auquel sera affecte
-	ce serveur*/
-	
-	serveur_appli(service);
+  serveur_appli(service);
 }
 
-
-/******************************************************************************/	
+/******************************************************************************/
 void serveur_appli(char *service)
 
 /* Procedure correspondant au traitemnt du serveur de votre application */
 
 {
 
-/* A completer ... */
-
+  int num_socket = h_socket(AF_INET, SOCK_STREAM);
+  struct sockaddr_in *adr;
+  adr_socket(service, NULL, SOCK_STREAM, &adr);
+  h_bind(num_socket, adr);
+  char buf[65535];
+  h_recvfrom(num_socket, buf, 65535, adr);
+  int reads = 1;
+  while (win && reads) {
+    char buffer[n];
+    reads = h_reads(num_socket, buffer, n);
+    /*jeu*/
+    int writes = h_writes(num_socket, tampon, len_tampon);
+  }
+  h_close(num_socket);
 }
 
-/******************************************************************************/	
-
+/******************************************************************************/
